52adc9ccf851340c6bc3bf6ed9111db8bd2c3f4b
Kris Sankaran <sankaran.kris@gmail.com>
2025-08-19 08:04:54 -0500

    update version

--- distortions/__init__.py
+++ distortions/__init__.py
@@ -5,10 +5,6 @@ Distortions: Interactive visualization of distortion in nonlinear embeddings.
 from . import geometry
 from . import visualization
 
-__version__ = "0.0.1"
-__author__ = "Kris Sankaran, Marina Meila"
-__license__ = "CC-BY-4.0"
-
 __all__ = [
     "geometry",
     "visualization"
